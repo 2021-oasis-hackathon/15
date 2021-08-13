14c758c0f1e7cdd5747041a0b721ee03be1d9575
posin2361 <62651440+posin2361@users.noreply.github.com>
2021-08-14 02:41:01 +0900

    메인화면소스
    main.html - 메인html
    main.css - 메인 기본적 css 설정
    main_section.css - section 부분 css(메인 내용 부분)
    test.html - 네이버 api 연동 목적 테스트용(인증 문제 발생)/추후에 변경 예정
    main_table.js - 메인 table부분 자바스크립트으로 동적으로 변경 수정중

---
+++ js/main_table.js
@@ -0,0 +1,16 @@
+var arr_zone = ['군산','익산','완주','진안'];//지역
+var start_area = 0;//시작가격
+var end_area = 1;//마지막가격
+var area_price = 1;//평단가
+
+function table_create(){//테이블 생성
+	document.write("<table><tr><td class="+"table_A>"+"지역"+"</td>");
+	//for(var num in arr_zone){
+		document.write("<td>"+arr_zone[1]+"</td></tr>");
+	//}
+	document.write("</table>");
+}
+
+function input(){//입력 받은 값 출력
+
+}
