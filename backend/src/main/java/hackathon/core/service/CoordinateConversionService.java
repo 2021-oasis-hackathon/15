c330c93c1bd85aff73a1245542e5c400066edf1e
jscheon <cjswltjr159@naver.com>
2021-08-18 19:28:32 +0900

    gitignore

--- backend/src/main/java/hackathon/core/service/CoordinateConversionService.java
+++ backend/src/main/java/hackathon/core/service/CoordinateConversionService.java
@@ -4,6 +4,7 @@ import hackathon.core.domain.Coordinates;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
@@ -13,13 +14,16 @@ import java.net.URLEncoder;
 
 
 public class CoordinateConversionService {
-    public Coordinates getJsonData(String address) throws Exception{
+    @Value("${kakaoApiKey}")
+    private String key;
+
+    public Coordinates getJsonData(String address) throws Exception {
 
         Coordinates coordinates = new Coordinates();
 
-        address = URLEncoder.encode(address,"UTF-8");
+        address = URLEncoder.encode(address, "UTF-8");
 
-        String url = "https://dapi.kakao.com/v2/local/search/address.json?query="+ address;
+        String url = "https://dapi.kakao.com/v2/local/search/address.json?query=" + address;
 
         String jsonString = new String();
 
@@ -27,27 +31,28 @@ public class CoordinateConversionService {
 
         URL Url = new URL(url);
 
+
         HttpsURLConnection conn = (HttpsURLConnection) Url.openConnection();
-        String auth ="KakaoAK " +"d35fcf8c11737e26a962c4acb8ad9e5f";
+        String auth = "KakaoAK " + key;
         conn.setRequestMethod("GET");
         conn.setRequestProperty("X-Requested-With", "curl");
         conn.setRequestProperty("Authorization", auth);
 
         BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-        while((buf = br.readLine()) != null) {
+        while ((buf = br.readLine()) != null) {
             jsonString += buf;
         }
 
         JSONParser paser = new JSONParser();
 
-        JSONObject J = (JSONObject)paser.parse(jsonString);
+        JSONObject J = (JSONObject) paser.parse(jsonString);
         JSONObject meta = (JSONObject) J.get("meta");
 
         JSONArray data = (JSONArray) J.get("documents");
         long size = (long) meta.get("total_count");
 
-        if(size > 0) {
-            JSONObject jsonX = (JSONObject)data.get(0);
+        if (size > 0) {
+            JSONObject jsonX = (JSONObject) data.get(0);
             coordinates.setX(Double.parseDouble(jsonX.get("x").toString()));
             coordinates.setY(Double.parseDouble(jsonX.get("y").toString()));
         }
